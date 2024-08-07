82d798f0f8c0f957e6abfbd6a2256ff873da3fe5
dineshsai14211 <136050667+dineshsai14211@users.noreply.github.com>
2024-08-07 15:45:33 +0530

    Delete apps directory

--- apps/constants.py
+++
@@ -1,6 +0,0 @@
-ACCESS_TOKEN = ""
-PAGE_ID = "372380875963234"
-
-HEADERS = {'Content-Type': 'application/json'}
-
-OPTION = "Enter which operation need to perform \n1)Make a post \n2)Make a post with photo \n3)Getting post information \n4)Delete a post \n5)Exit = "
